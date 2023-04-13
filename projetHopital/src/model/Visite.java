a3d831e5e00fc45620b041dc6e4223ee23abd6f0
Anton-mnc <antoine.mgy.mnc@gmail.com>
2023-04-13 14:38:40 +0200

    Package model create with class

---
+++ projetHopital/src/model/Visite.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Visite {
+	private int id;
+	private int idSecu;
+	private String nomMedecin;
+	private double cout;
+	private LocalDate date;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIdSecu() {
+		return idSecu;
+	}
+
+	public void setIdSecu(int idSecu) {
+		this.idSecu = idSecu;
+	}
+
+	public String getNomMedecin() {
+		return nomMedecin;
+	}
+
+	public void setNomMedecin(String nomMedecin) {
+		this.nomMedecin = nomMedecin;
+	}
+
+	public double getCout() {
+		return cout;
+	}
+
+	public void setCout(double cout) {
+		this.cout = cout;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Visite(int id, int idSecu, String nomMedecin, double cout, LocalDate date) {
+		super();
+		this.id = id;
+		this.idSecu = idSecu;
+		this.nomMedecin = nomMedecin;
+		this.cout = cout;
+		this.date = date;
+	}
+
+	public Visite() {
+		super();
+	}
+
+}
