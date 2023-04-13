bd3915c3fba16005ff6383972fe0a6de635767f8
Laure Le Nouail <laure.lenouail@gmail.com>
2023-04-13 18:49:58 +0200

    ecriture du menu secretaire

--- projetHopital/src/user/Test.java
+++ projetHopital/src/user/Test.java
@@ -1,30 +1,16 @@
 package user;
 
+import model.Personnel;
+
 public class Test {
 
 	public static void main(String[] args) {
-		int role = authentification();
-		
-		if(role == 1 || role == 2){
-			menuMedecin();
-		}
-		if(role == 0){
-			menuSecretaire();
-		}
+		authentification();
 
 	}
 	
-	public static int authentification(){
+	public static Personnel authentification(){
 		
-		return 1;
-	}
-	
-	public static void menuMedecin(){
-		String menu = "1.";
-	}
-	
-	public static void menuSecretaire(){
-		authentification();
-		String menu = "1.";
+		return null;
 	}
 }
